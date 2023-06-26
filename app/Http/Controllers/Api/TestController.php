8ba70f69afe87183af036b38d998f17ede772b29
Risqi Ardiansyah <ardiris19@gmail.com>
2023-06-26 17:08:12 +0700

    image access

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -5,12 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class TestController extends ApiController
 {
     public function __construct()
     {
+    }
+
+    public function getImage($folder = '', $img)
+    {
+        $file_name = $img;
+        $storagePath  = Storage::disk('public')->getDriver()->getAdapter()->getPathPrefix();
+        $fullPath = $storagePath . '/';
+        if ($folder) {
+            $fullPath .= $folder.'/';
+        }
+        $fullPath .= $file_name;
+        
+        return $this->sendResponseFile($fullPath);
     }
 
     public function test(Request $request)
